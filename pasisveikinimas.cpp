a8cfa98109d404e8a980631e97b3bd91106e76c6
grovcat <47323014+grovcat@users.noreply.github.com>
2019-02-11 15:04:05 +0200

    Add files via upload

---
+++ pasisveikinimas.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int fulllenght;
+    cout << "Koks jusu vardas" << endl;
+    cin >> name;
+    fulllenght = 10 + name.size() + 4;
+    for(int i = 0; i != fulllenght; i++)
+    {
+        printf("%c", '*');
+    }
+    printf("\n");
+
+    printf("%*c%c\n", -(fulllenght-1), '*', '*');
+    printf("%*c%s%s%c%*c\n", -2, '*',"Sveikas, ", name.c_str(), '!', 2, '*');
+    printf("%*c%c\n", -(fulllenght-1), '*', '*');
+
+    for(int i = 0; i != fulllenght; i++)
+    {
+        printf("%c", '*');
+    }
+    printf("\n");
+    return 0;
+}
